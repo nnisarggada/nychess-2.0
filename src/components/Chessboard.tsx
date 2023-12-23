221ccc054ed860683445d747a0dce701ad3585b6
nnisarggada <gadannisarg@gmail.com>
2023-12-24 04:07:14 +0530

    init

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -3,56 +3,44 @@ import { useState, useEffect } from "react";
 
 export default function Chessboard() {
 
-  const squares = ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"];
+  const squares = [
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+  ];
+
   const squareElements = [];
   const squareColors = ["bg-[#779556]", "bg-[#ebecd0]"];
   let squareColor = 0;
   let rounded = "";
-  const pieceImages = {
-    "k": "/images/pieces/wK.png",
-    "q": "/images/pieces/wQ.png",
-    "r": "/images/pieces/wR.png",
-    "b": "/images/pieces/wB.png",
-    "n": "/images/pieces/wN.png",
-    "p": "/images/pieces/wP.png",
-    "K": "/images/pieces/bK.png",
-    "Q": "/images/pieces/bQ.png",
-    "R": "/images/pieces/bR.png",
-    "B": "/images/pieces/bB.png",
-    "N": "/images/pieces/bN.png",
-    "P": "/images/pieces/bP.png",
-  }
-  const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 
-  const loadFen = (fen: string) => {
-    const board = document.getElementById("board");
-    if (!board) return;
+  const board = brain.board;
 
-    const ranks = fen.split(' ')[0].split('/');
-    const squares = board.querySelectorAll('.square');
-
-    let squareIndex = 0;
-
-    for (let i = 0; i < ranks.length; i++) {
-      const rank = ranks[i];
-
-      for (let j = 0; j < rank.length; j++) {
-        const piece = rank[j];
-        let num = parseInt(piece);
-        if (!isNaN(num)) {
-          squareIndex += num;
-        } else {
-          const square = squares[squareIndex];
-          square.innerHTML = `<img className="piece absolute top-0 left-0 w-full h-full z-50" src="${pieceImages[piece as keyof typeof pieceImages]}" alt="${piece}">`;
-          squareIndex++;
-        }
+  const loadBoard = (boardArray: Array<string>) => {
+    for (let i = 0; i < boardArray.length; i++) {
+      if (boardArray[i] == "") {
+        continue;
+      }
+      const square = squares[i];
+      const src = `/images/pieces/${boardArray[i]}.png`;
+      const squareElement = document.getElementById(square);
+      if (squareElement) {
+        const img = document.createElement("img");
+        img.setAttribute("class", "piece-img");
+        img.setAttribute("src", src);
+        squareElement.appendChild(img);
       }
     }
   };
 
   useEffect(() => {
-    loadFen(fen);
-  }, [fen]);
+    loadBoard(board);
+  })
 
   for (let i = 0; i < squares.length; i++) {
     const square = squares[i];
@@ -85,7 +73,7 @@ export default function Chessboard() {
   }
 
   return (
-    <section id="board" className="w-full aspect-square bg-neutral-700 flex flex-wrap-reverse rounded overflow-hidden">
+    <section id="board" className="w-full aspect-square bg-neutral-700 flex flex-wrap rounded overflow-hidden">
       {squareElements}
     </section>
   );
--- src/scripts/brain.ts
+++ src/scripts/brain.ts
@@ -8,18 +8,18 @@ function fenToArray(fen: string) {
       if (isNaN(parseInt(char, 10))) {
         // If it's a piece
         const pieceMapping = {
-          'r': 'wR',
-          'n': 'wN',
-          'b': 'wB',
-          'q': 'wQ',
-          'k': 'wK',
-          'p': 'wP',
-          'R': 'bR',
-          'N': 'bN',
-          'B': 'bB',
-          'Q': 'bQ',
-          'K': 'bK',
-          'P': 'bP',
+          'R': 'wR',
+          'N': 'wN',
+          'B': 'wB',
+          'Q': 'wQ',
+          'K': 'wK',
+          'P': 'wP',
+          'r': 'bR',
+          'n': 'bN',
+          'b': 'bB',
+          'q': 'bQ',
+          'k': 'bK',
+          'p': 'bP',
         };
 
         const mappedPiece = pieceMapping[char as keyof typeof pieceMapping] || char;
@@ -35,53 +35,109 @@ function fenToArray(fen: string) {
   return boardArray;
 }
 
+function squareToIndex(square: string,) {
+  const file = square.charAt(0).charCodeAt(0) - 'a'.charCodeAt(0);
+  const rank = 8 - parseInt(square.charAt(1), 10);
+
+  return rank * 8 + file;
+}
+
 // Vars for init
-const startingFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+const startingFen = "rnbqkbnr/pppppppp/8/2P2B2/1B2P3/1P1N1PN1/P2Q2PP/R3K2R w KQkq - 0 1";
 
 // Init
-let board = fenToArray(startingFen);
-let userClicksLeft = 0;
+export let board = fenToArray(startingFen);
+let userFirstClick = true;
 let selectedSquare = "";
 let selectedPiece = "";
+let whiteToMove = true;
+
+let validSquares = [];
+let validPawnSquares: string[] = [];
+
+// Logic
+
+const generatePawnMoves = (square: string) => {
+  const validSquares = [];
+  const squareIndex = squareToIndex(square);
+  const piece = board[squareIndex];
+  const pieceColor = piece[0];
+  if (pieceColor == 'w') {
+    if (board[squareToIndex(square[1] + 1)])
+      validSquares.push(square[0] + (parseInt(square[1]) + 1));
+    if (parseInt(square[1]) == 2) {
+      validSquares.push(square[0] + (parseInt(square[1]) + 2));
+    }
+  } else {
+  }
+
+  validPawnSquares.push(...validSquares);
+}
+
 
 // Helper Funcs
 export const handleClick = (square: string) => {
 
-  const file = square.charAt(0).charCodeAt(0) - 'a'.charCodeAt(0);
-  const rank = 8 - parseInt(square.charAt(1), 10);
+  if (selectedSquare == square) {
+    return;
+  }
 
-  if (file >= 0 && file < 8 && rank >= 0 && rank < 8) {
-    const index = rank * 8 + file;
-    const pieceOnSquare = board[63 - index];
+  if (userFirstClick) {
 
-    userClicksLeft ^= 1;
+    selectedPiece = board[squareToIndex(square)];
 
-    if (userClicksLeft !== 0 || selectedSquare === square) {
-      selectedSquare = square;
-      selectedPiece = pieceOnSquare;
-      console.log(selectedPiece);
-      // Show available moves
+    console.log(square, selectedPiece);
 
-    } else {
-      // Verify valid move
-      const oldSquare = document.getElementById(selectedSquare);
-      const imgElement = oldSquare?.querySelector("img");
-      oldSquare?.removeChild(imgElement!);
-      const newSquare = document.getElementById(square);
-      const newImg = newSquare?.querySelector("img");
-      const src = `/images/pieces/${selectedPiece}.png`;
-      if (newImg) {
-        newImg.setAttribute("src", src);
-      }
-      else {
-        const createdImg = document.createElement("img");
-        createdImg.setAttribute("src", src);
-        newSquare?.appendChild(createdImg);
-      }
-      selectedSquare = "";
-      selectedPiece = "";
-      console.log(selectedPiece);
+    if (selectedPiece == "") {
+      return;
     }
 
+    const squareElement = document.getElementById(square);
+    squareElement?.classList.add('bg-amber-200');
+    selectedSquare = square;
+
+    if (selectedPiece[1] == 'P') {
+      generatePawnMoves(square);
+      validPawnSquares?.forEach(square => {
+        const squareElement = document.getElementById(square);
+        const circle = document.createElement("div");
+        circle.classList.add("move-circle", "absolute", "top-1/2", "left-1/2", "-translate-x-1/2", "-translate-y-1/2", "rounded-full", "w-1/3", "aspect-square", "bg-black/30");
+        squareElement?.appendChild(circle);
+      })
+    }
+
+    // Show available moves
+
+  } else {
+
+    const guides = document.querySelectorAll(".move-circle");
+    guides.forEach(guide => guide.remove());
+
+    const oldSquare = document.getElementById(selectedSquare);
+    oldSquare?.classList.remove('bg-amber-200');
+
+    if (selectedPiece[1] == "P" && !validPawnSquares.includes(square)) {
+      selectedPiece = "";
+      selectedSquare = "";
+      userFirstClick = true;
+      validPawnSquares = [];
+      return;
+    }
+
+    validPawnSquares = [];
+
+    board[squareToIndex(square)] = selectedPiece;
+    board[squareToIndex(selectedSquare)] = "";
+
+    oldSquare?.querySelectorAll(".piece-img").forEach(img => img.remove());
+    const newSquare = document.getElementById(square);
+    newSquare?.querySelectorAll(".piece-img").forEach(img => img.remove());
+    const url = `/images/pieces/${selectedPiece}.png`;
+    const img = document.createElement("img");
+    img.setAttribute("class", "piece-img");
+    img.setAttribute("src", url);
+    newSquare?.appendChild(img);
   }
+
+  userFirstClick = !userFirstClick;
 }
